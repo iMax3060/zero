48a4653ce5dcfa8f025ef8b557966d45adeaf8d3
Max Gilbert <m_gilbert13@cs.uni-kl.de>
2017-02-14 19:05:10 +0100

    First release of page eviction strategy CART.

--- src/sm/bf_tree.cpp
+++ src/sm/bf_tree.cpp
@@ -146,10 +146,6 @@ bf_tree_m::bf_tree_m(const sm_options& options)
     _cleaner_decoupled = options.get_bool_option("sm_cleaner_decoupled", false);
     
     _logstats_fix = options.get_bool_option("sm_fix_stats", false);
-    
-    if (!_evictioner) {
-        get_evictioner();
-    }
 }
 
 void bf_tree_m::shutdown()
@@ -293,9 +289,7 @@ w_rc_t bf_tree_m::fix(generic_page* parent, generic_page*& page,
 
         cb.pin(); //LL: if calling pin, latch should be in EX mode always, no?
         cb.inc_ref_count();
-        if (_evictioner) {
-            _evictioner->ref(idx);
-        }
+        get_evictioner()->ref(idx);
         if (mode == LATCH_EX) {
             cb.inc_ref_count_ex();
         }
@@ -395,10 +389,8 @@ w_rc_t bf_tree_m::fix(generic_page* parent, generic_page*& page,
                 }
                 cb.init(pid, page->lsn);
             }
-	
-	        if (_evictioner) {
-                _evictioner->miss_ref(idx, pid);
-            }
+    
+            get_evictioner()->miss_ref(idx, pid);
 
             w_assert1(_is_active_idx(idx));
 
@@ -434,7 +426,7 @@ w_rc_t bf_tree_m::fix(generic_page* parent, generic_page*& page,
             w_assert1(_is_active_idx(idx));
             cb.pin();
             cb.inc_ref_count();
-            if(_evictioner) _evictioner->ref(idx);
+            get_evictioner()->ref(idx);
             if (mode == LATCH_EX) {
                 cb.inc_ref_count_ex();
             }
